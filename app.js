196f2ce90cec7a44b7e49a3c19457e2d67d0d49d
Tri <caominhtri91@gmail.com>
2017-03-26 00:56:25 +0700

    Seed product datas and show it on index

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
+var mongoose = require('mongoose');
+var session = require('express-session');
 
 var index = require('./routes/index');
 
 var app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/shopping');
 
 // view engine setup
 app.engine('.hbs', expressHbs({
@@ -23,19 +28,20 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'mySecret', resave: false, saveUninitialized: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
+var csrf = require('csurf');
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping cart' });
+  Product.find().then((products) => {
+    var productChunks = [];
+    var chunkSize = 1;
+    products.forEach((product) => {
+      if (chunkSize%3 === 0) {
+        productChunks.push(products.slice(chunkSize - 3, chunkSize));
+      }
+      chunkSize++;
+    });
+    res.render('shop/index', { title: 'Shopping cart', products: productChunks});
+  }).catch((err) => res.status(404).send());
+});
+
+router.get('/user/signup', (req, res, next) => {
+  res.render('user/signup', { csrfToken: req.csrfToken() });
+});
+
+router.post('/user/signup', (req, res, next) => {
+  res.redirect('/');
 });
 
 module.exports = router;
